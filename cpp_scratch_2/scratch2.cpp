45e941b02125f1d95fec06aea09b694490beb18c
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-10-12 17:15:11 -0500

    Playing with Cin and Const

--- cpp_scratch_2/scratch2.cpp
+++ cpp_scratch_2/scratch2.cpp
@@ -15,27 +15,39 @@ int main(){
     bool Stop = false;
     string user_selection;
 
-    cout<<"Bool variable Go: "<<Go<<endl;
-    cout<<"Bool variable Stop: "<<Stop<<endl;
+    // cout<<"Bool variable Go: "<<Go<<endl;
+    // cout<<"Bool variable Stop: "<<Stop<<endl;
 
-    cout<<"Enter Go or Stop: ";
-    cin >> user_selection;
+    // cout<<"Enter Go or Stop: ";
+    // cin >> user_selection;
 
-    cout<<"You entered: "<<user_selection<<endl;
+    // cout<<"You entered: "<<user_selection<<endl;
 
     //create an equivalence test for the string entered
 
-    if (user_selection == "Go"){
-        cout<<"Let's Go"<<endl;
-    }
-    else if (user_selection == "Stop"){
-        cout <<"You Entered Stop"<<endl;
-    }
-    else{
-        cout<<"I don't understand your entry"<<endl;
-    }
+    // if (user_selection == "Go"){
+    //     cout<<"Let's Go"<<endl;
+    // }
+    // else if (user_selection == "Stop"){
+    //     cout <<"You Entered Stop"<<endl;
+    // }
+    // else{
+    //     cout<<"I don't understand your entry"<<endl;
+    // }
 
+    cout<< "Let's play with Const"<<endl;
+
+    const int Ten = 10;
+    //cin >> int user_int; this dosen't work.
+    cout <<"The int Ten is: "<<Ten<<endl;
+    // cout<<"Let's look at the result of Ten+=10:"<<endl;
+    // Ten += 10;
+    // cout<<"Ten after manipulation: "<< Ten<<endl;
+
+    //doing Ten+= 10; errors out the program.
     
+
+
 
     return 0;
 }
