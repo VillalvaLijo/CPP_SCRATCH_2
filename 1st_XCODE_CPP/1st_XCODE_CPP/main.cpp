d6db19a1bf2948547434786a62a1e5ef7b074e4b
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-10-14 17:15:41 -0500

    Playing with type casting

---
+++ 1st_XCODE_CPP/1st_XCODE_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1st_XCODE_CPP
+//
+//  Created by Samuel Villalva Lijo on 10/13/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- cpp_scratch_2/scratch2.cpp
+++ cpp_scratch_2/scratch2.cpp
@@ -46,14 +46,33 @@ int main(){
 
     //doing Ten+= 10; errors out the program.
     
-    const int Lbs_per_stn = 14;
-    int lbs;
-    cout << "Enter your weight in pounds: ";
-    cin >> lbs;
+    //const int Lbs_per_stn = 14;
+    //int lbs;
+    //cout << "Enter your weight in pounds: ";
+    //cin >> lbs;
 
-    int stone = lbs / Lbs_per_stn; //whole stone...CPP rounds
-    int pounds = lbs % Lbs_per_stn; // remander in pounds
-    cout << lbs << "pounds are " << stone << "stone, "<<pounds<<" pound(s).\n";
+    //int stone = lbs / Lbs_per_stn; //whole stone...CPP rounds
+    //int pounds = lbs % Lbs_per_stn; // remander in pounds
+    //cout << lbs << "pounds are " << stone << "stone, "<<pounds<<" pound(s).\n";
+
+    int auks, bats, coots;
+
+    //the following statement adds the values as double,
+    //then converts the result to int
+    auks = 19.99 + 11.99;
+
+    //these statements add values as int
+    bats = (int) 19.99 + (int) 11.99; //old C syntax
+    coots = int (19.99) + int (11.99); // new CPP syntax
+    cout << "auks = "<<auks<<", bats= "<<bats;
+    cout << ", coots = " << coots << endl;
+
+    char ch = 'z';
+    cout <<"The code for "<< ch << " is ";
+    cout << int(ch) << endl;
+    cout << "Yes, the code is ";
+    cout << static_cast<int>(ch) << endl;
+
 
 
     return 0;
